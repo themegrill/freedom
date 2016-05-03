76d6acbaa75c6785242e34de7cf24b31856aaa6d
Bishal Napit <mebishalnapit@gmail.com>
2016-05-03 15:14:23 +0545

    Fixing done for JSHint Linting errors

--- js/freedom_customizer.js
+++ js/freedom_customizer.js
@@ -1,10 +1,9 @@
-/**
- * Theme Customizer related js
- */
-
+/* global freedom_customizer_obj */
 jQuery(document).ready(function() {
-
-   jQuery('#customize-info .preview-notice').append(
+	/**
+	 * Theme Customizer related js
+	 */
+	jQuery('#customize-info .preview-notice').append(
 		'<a class="themegrill-pro-info" href="http://themegrill.com/themes/freedom-pro/" target="_blank">{pro}</a>'
 		.replace('{pro}',freedom_customizer_obj.pro));
 
