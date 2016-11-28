6aefe4cbf34befaad08bfcf8c7c5c6701d0b284f
rabindratharu <rabindra.tharu.np@gmail.com>
2016-11-28 15:38:27 +0545

    Migrates the theme custom css box settings to additional css section introuduced in WordPress 4.7

--- inc/functions.php
+++ inc/functions.php
@@ -294,7 +294,7 @@ function freedom_custom_css() {
 	}
 
 	$freedom_custom_css = get_theme_mod( 'freedom_custom_css', '' );
-	if( !empty( $freedom_custom_css ) ) {
+	if( !empty( $freedom_custom_css ) && ! function_exists( 'wp_update_custom_css_post' ) ) {
 		echo '<!-- '.get_bloginfo('name').' Custom Styles -->';
 		?><style type="text/css"><?php echo $freedom_custom_css; ?></style><?php
 	}
@@ -475,4 +475,26 @@ if ( ! function_exists( 'freedom_the_custom_logo' ) ) {
     }
   }
 }
-?>
+
+/**************************************************************************************/
+
+/**
+ * Migrate any existing theme CSS codes added in Customize Options to the core option added in WordPress 4.7
+ */
+function freedom_customm_css_migrate() {
+	if ( get_option( 'freedom_custom_css_transfer' ) ) {
+		return;
+	}
+	$theme_custom_css = get_theme_mod( 'freedom_custom_css', '' );
+	if ( ! empty( $theme_custom_css ) && function_exists( 'wp_update_custom_css_post' ) ) {
+		$wordpress_core_css = wp_get_custom_css(); // Preserve any CSS already added to the core option.
+		$return = wp_update_custom_css_post( $wordpress_core_css . $theme_custom_css );
+		if ( ! is_wp_error( $return ) ) {
+			// Set the transfer as complete
+			update_option( 'freedom_custom_css_transfer', 1 );
+			// Remove the old theme_mod option for the Custom CSS Box provided via theme
+			remove_theme_mod( 'freedom_custom_css' );
+		}
+	}
+}
+add_action( 'after_setup_theme', 'freedom_customm_css_migrate' );
