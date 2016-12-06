ec9107319261ae4dd82a1908d8b70565e03db8bd
Romos <romosamatya17@gmail.com>
2016-12-06 10:08:52 +0545

    Adds responsive menu design css.

--- js/freedom-custom.js
+++ js/freedom-custom.js
@@ -1,18 +1,32 @@
-jQuery(document).ready(function(){
-	jQuery('#scroll-up').hide();
-	jQuery(function () {
-		jQuery(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 1000) {
-				jQuery('#scroll-up').fadeIn();
-			} else {
-				jQuery('#scroll-up').fadeOut();
-			}
-		});
-		jQuery('a#scroll-up').click(function () {
-			jQuery('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
+jQuery(document).ready(function() {
+    jQuery('#scroll-up').hide();
+    jQuery(function() {
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > 1000) {
+                jQuery('#scroll-up').fadeIn();
+            } else {
+                jQuery('#scroll-up').fadeOut();
+            }
+        });
+        jQuery('a#scroll-up').click(function() {
+            jQuery('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+});
+
+jQuery(document).ready(function() {
+    jQuery('.better-responsive-menu #site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="fa fa-caret-down"></i> </span>');
+    jQuery('.better-responsive-menu #site-navigation .sub-toggle').click(function() {
+        jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
+        jQuery(this).children('.fa-caret-right').first().toggleClass('fa-caret-down');
+        jQuery(this).toggleClass('active');
+    });
+
+    jQuery('#site-navigation .menu-toggle').click(function() {
+      jQuery('#site-navigation .menu').slideToggle('slow');
+    });
+
 });
