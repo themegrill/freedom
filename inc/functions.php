7085099a7be79a9ccedde7af8c6fd68581651a6b
rabindratharu <rabindra.tharu.np@gmail.com>
2016-12-20 15:17:23 +0545

    added WooCommerce support

--- inc/functions.php
+++ inc/functions.php
@@ -463,4 +463,23 @@ function freedom_sanitize_textarea_custom( $input,$option ) {
    }
    return $output;
 }
-?>
+
+/**
+ * Making the theme Woocommrece compatible
+ */
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+
+add_filter( 'woocommerce_show_page_title', '__return_false' );
+
+add_action('woocommerce_before_main_content', 'freedom_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'freedom_wrapper_end', 10);
+
+function freedom_wrapper_start() {
+  echo '<div id="primary">';
+}
+
+function freedom_wrapper_end() {
+  echo '</div>';
+}
