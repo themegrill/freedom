cc952ca43264af9a035b95289f4fb00e35dd37c5
rabindratharu <rabindra.tharu.np@gmail.com>
2016-12-21 14:15:15 +0545

    properly enqueue IE support script

--- inc/functions.php
+++ inc/functions.php
@@ -50,10 +50,8 @@ function freedom_scripts_styles_method() {
 
 	wp_enqueue_style( 'google_fonts' );
 
-   $freedom_user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
-	if(preg_match('/(?i)msie [1-8]/',$freedom_user_agent)) {
-		wp_enqueue_script( 'html5', FREEDOM_JS_URL . '/html5shiv.min.js', true );
-	}
+	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/js/html5shiv.js', array(), '3.7.3', false );
+	wp_script_add_data( 'html5shiv', 'conditional', 'lte IE 8' );
 
 }
 
