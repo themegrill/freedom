da8304e365648a1181f7a17f19b889d291a0fbfa
Nitu Shrestha <shresthanitu984@gmail.com>
2018-05-30 14:32:24 +0545

    Added - Function for related posts

--- inc/functions.php
+++ inc/functions.php
@@ -610,3 +610,46 @@ function freedom_wrapper_start() {
 function freedom_wrapper_end() {
 	echo '</div>';
 }
+
+if ( ! function_exists( 'freedom_related_posts_function' ) ) {
+
+	/**
+	 * Display the related posts
+	 */
+	function freedom_related_posts_function() {
+			wp_reset_postdata();
+			global $post;
+
+			// Define shared post arguments
+			$args = array(
+				'no_found_rows'          => true,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
+				'ignore_sticky_posts'    => 1,
+				'orderby'                => 'rand',
+				'post__not_in'           => array( $post->ID ),
+				'posts_per_page'         => 3,
+			);
+			// Related by categories
+			if ( get_theme_mod('the_newsmag_related_posts', 'categories' ) == 'categories' ) {
+				$cats                 = wp_get_post_categories( $post->ID, array( 'fields' => 'ids' ) );
+				$args['category__in'] = $cats;
+			}
+
+			// Related by tags
+			if ( get_theme_mod( 'the_newsmag_related_posts', 'categories' ) == 'tags' ) {
+				$tags            = wp_get_post_tags( $post->ID, array( 'fields' => 'ids' ) );
+				$args['tag__in'] = $tags;
+
+				// If no tags added, return
+				if ( ! $tags ) {
+					$break = true;
+				}
+			}
+
+			$query = ! isset( $break ) ? new WP_Query( $args ) : new WP_Query;
+
+			return $query;
+	}
+
+}
