d8fde8b7f9df65f362dff3879b78b60904c2bdeb
Bishal Napit <mebishalnapit@gmail.com>
2019-08-13 16:10:59 +0545

    Added - Skip to content link

--- header.php
+++ header.php
@@ -32,6 +32,8 @@ if ( function_exists( 'wp_body_open' ) ) {
 
 <?php do_action( 'before' ); ?>
 <div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'freedom' ); ?></a>
+
 	<?php do_action( 'freedom_before_header' ); ?>
 	<header id="masthead" class="site-header clearfix">
 		<div id="header-text-nav-container" class="clearfix">
